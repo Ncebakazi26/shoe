93f87295f92ef02379bedfbed405030d6c90a97f
Ncebakazi <ncebakazi414@gmail.com>
2021-12-09 12:20:22 +0200

    shoes

---
+++ shoe-catalogue.js
@@ -0,0 +1,19 @@
+var shoes = [
+    {Brand: "Nike",Color:"Black", Size:1},
+    {Brand: "Nike",Color:"Black", Size:1},
+    {Brand: "Nike",Color:"Black", Size:1},
+    {Brand: "Nike",Color:"Black", Size:1},
+    {Brand: "Nike",Color:"Black", Size:1},
+    {Brand: "Nike",Color:"Black", Size:1},
+    {Brand: "Nike",Color:"Black", Size:1},
+    {Brand: "Nike",Color:"Black", Size:1},
+    {Brand: "Nike",Color:"Black", Size:1},
+    {Brand: "Nike",Color:"Black", Size:1},
+    {Brand: "Nike",Color:"Black", Size:1},
+    {Brand: "Nike",Color:"Black", Size:1},
+    {Brand: "Nike",Color:"Black", Size:1},
+
+
+
+
+];
